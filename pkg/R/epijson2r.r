f32effa183b4563fbf249dc4d69ba8feb38d0c07
AndySouth <southandy@gmail.com>
2015-02-11 14:58:47 +0000

    add comments about return list

--- pkg/R/epijson2r.r
+++ pkg/R/epijson2r.r
@@ -6,7 +6,7 @@
 #' @param file an epijson filename to convert to R
 #' #@param file optional filename for JSON string
 #' 
-#' @return ?maybe a string of the JSON
+#' @return a list of lists of the epijson content
 #' @export
 #' 
 
@@ -19,7 +19,19 @@ epijson2r <- function(file)
   
   ## convert from json to list
   listjson <- fromJSON(file)
+ 
   
+# this might be needed later to get data into a dataframe
+# suitable for an obkData object
+#   ## get data into a data.frame ##
+#   ## get all fields
+#   temp <- lapply(listjson, unlist, recursive=TRUE)
+#   allfields <- unique(unlist(lapply(temp, names)))
+#   
+#   ## get data into a data.frame
+#   dFindividuals <- matrix(unlist(lapply(temp, f1)), nrow=length(listjson), byrow=TRUE)
+#   dFindividuals <- as.data.frame(dFindividuals)
+#   names(dFindividuals) <- allfields
   
   
   return(listjson)
