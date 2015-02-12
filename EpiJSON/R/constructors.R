18aa60666468f58923838e69fba22ed4c6beeaa2
EllieSherrardSmith <e.sherrard-smith@imperial.ac.uk>
2015-02-12 14:17:05 +0000

    updates to the definitions and examples for these functions

--- EpiJSON/R/constructors.R
+++ EpiJSON/R/constructors.R
@@ -177,7 +177,7 @@ createIndividual <- function(id, attributes, records){
 #'                                                                                             name="rec2contact",
 #'                                                                                             date=as.Date(dF$rec2date),
 #'                                                                                             location="",
-#'                                                                                             attributes=list(createAttribute(name="rec2risk",type="str",value=list(dF$rec1risk)))
+#'                                                                                             attributes=list(createAttribute(name="rec2risk",type="str",value=list(dF$rec2risk)))
 #'                                                                                            )
 #'                                                                                )  
 #'                                                                            )
@@ -185,7 +185,7 @@ createIndividual <- function(id, attributes, records){
 #'                    ) 
 #'                   
 #'               
-#' @return an \code{\link{ejRecord}} object
+#' @return an \code{\link{ejObject}} object
 #' @export
 #' 
 
@@ -200,6 +200,41 @@ createEJObject <- function(metadata, individuals){
 	), class="ejObject")
 }
 
+#' 
+#' This function defines epiJSON Metadata 
+#' output \code{ejMetadata}  
+#' 
+#' @param attributes These are the attributes of the metadata
+#' 
+#' @example
+#' dF<- data.frame(id=c("A","B","3D"),
+#'                 name=c("tom","andy","ellie"),
+#'                 dob=c("1984-03-14","1985-11-13","1987-06-16"),
+#'                 gender=c("male","male","female"),
+#'                 rec1contact=c(2,1,5),
+#'                 rec1date=c("2014-12-28","2014-12-29","2015-01-03"),
+#'                 rec1risk=c("high","high","low"),  
+#'                 rec1temp=c(39,41,41),
+#'                 rec2contact=c(4,1,1),
+#'                 rec2date=c("2015-01-02","2015-01-12","2015-01-09"),
+#'                 rec2risk=c("high","low","high"))
+#' 
+#' metadata1<-createMetadata (attributes=list(createAttribute(name="name",type="str",value=list(dF$name)),
+#'                                            createAttribute(name="dob",type="date",value=list(as.Date(dF$dob))),
+#'                                            createAttribute(name="gender",type="str",value=list(dF$gender)),
+#'                                            createAttribute(name="rec1risk",type="str",value=list(dF$rec1risk)),
+#'                                            createAttribute(name="rec1temp",type="int",value=list(dF$rec1temp)),
+#'                                            createAttribute(name="rec2risk",type="str",value=list(dF$rec2risk))  ))
+#'                                            
+#'                              
+#'               
+#' @return an \code{\link{ejMetadata}} object
+#' @export
+#' 
+
+#' Create Metadata
+#' @export
+#' 
 
 createMetadata <- function(attributes){
 	structure(attributes, class="ejMetadata")
